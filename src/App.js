88b748404e575d5de7483002e907d31c1d739de4
camalzadeelvin <camalzadeelvin@STHQ0122-01>
2023-07-27 13:38:14 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Shapes from "./Shapes";
 
+let nextId = 0;
 function App() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  function addArtist() {
+    // artists.push({
+    //   id:++nextId,
+    //   name:name
+    // });
+
+    // setArtists([...artists, {
+    //   id: ++nextId,
+    //   name: name,
+    // }]);
+
+    setArtists([{
+      id: ++nextId,
+      name: name,
+    },...artists]);
+
+    console.log(artists);
+  }
+
+  function removeItem(a){
+    setArtists(artists.filter(artist=>artist.id!==a.id));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>Our artists</h1>
+      <section>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={addArtist}>Add</button>
+      </section>
+      <ul>
+        {artists.map((a) => (
+          <li key={a.id}><span><b>{a.id}</b> </span> : {a.name} 
+          <button onClick={(e)=>{
+            removeItem(a);
+          }}>Delete</button>
+          </li>
+        ))}
+      </ul> */}
+      <Shapes></Shapes>
     </div>
   );
 }
